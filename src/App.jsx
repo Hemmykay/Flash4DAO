3b14d06cd44d9a6b6aa6ef4b4bee2d505d5d879c
Kehinde Fayomi <42616193+Hemmykay@users.noreply.github.com>
2022-06-27 22:49:13 +0000

    Display members address and token quantity

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useAddress, useMetamask, useEditionDrop } from '@thirdweb-dev/react';
-import { useState, useEffect } from 'react';
+import { useAddress, useMetamask, useEditionDrop, useToken } from '@thirdweb-dev/react';
+import { useState, useEffect, useMemo } from 'react';
 
 const App = () => {
   // Use the hooks thirdweb give us.
@@ -9,10 +9,76 @@ const App = () => {
 
   // Initialize our editionDrop contract
   const editionDrop = useEditionDrop("0x704e495ede1CcFCa793Ffc9c444dfFFE7585353b");
+
+  const token = useToken("0xb96F8d2279D940AD93c7D45ea28fA0fe4F41172C")
   // State variable for us to know if user has our NFT.
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
   // isClaiming lets us easily keep a loading state while the NFT is minting.
   const [isClaiming, setIsClaiming] = useState(false);
+
+  // Holds the amount of token each member has in state.
+const [memberTokenAmounts, setMemberTokenAmounts] = useState([]);
+// The array holding all of our members addresses.
+const [memberAddresses, setMemberAddresses] = useState([]);
+
+// A fancy function to shorten someones wallet address, no need to show the whole thing. 
+const shortenAddress = (str) => {
+  return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+};
+
+// This useEffect grabs all the addresses of our members holding our NFT.
+useEffect(() => {
+  if (!hasClaimedNFT) {
+    return;
+  }
+
+  // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
+  // with tokenId 0.
+  const getAllAddresses = async () => {
+    try {
+      const memberAddresses = await editionDrop.history.getAllClaimerAddresses(0);
+      setMemberAddresses(memberAddresses);
+      console.log("🚀 Members addresses", memberAddresses);
+    } catch (error) {
+      console.error("failed to get member list", error);
+    }
+
+  };
+  getAllAddresses();
+}, [hasClaimedNFT, editionDrop.history]);
+
+// This useEffect grabs the # of token each member holds.
+useEffect(() => {
+  if (!hasClaimedNFT) {
+    return;
+  }
+
+  const getAllBalances = async () => {
+    try {
+      const amounts = await token.history.getAllHolderBalances();
+      setMemberTokenAmounts(amounts);
+      console.log("👜 Amounts", amounts);
+    } catch (error) {
+      console.error("failed to get member balances", error);
+    }
+  };
+  getAllBalances();
+}, [hasClaimedNFT, token.history]);
+
+// Now, we combine the memberAddresses and memberTokenAmounts into a single array
+const memberList = useMemo(() => {
+  return memberAddresses.map((address) => {
+    // We're checking if we are finding the address in the memberTokenAmounts array.
+    // If we are, we'll return the amount of token the user has.
+    // Otherwise, return 0.
+    const member = memberTokenAmounts?.find(({ holder }) => holder === address);
+
+    return {
+      address,
+      tokenAmount: member?.balance.displayValue || "0",
+    }
+  });
+}, [memberAddresses, memberTokenAmounts]);
 
   useEffect(() => {
     // If they don't have an connected wallet, exit!
@@ -57,7 +123,7 @@ const App = () => {
    if (!address) {
     return (
       <div className="landing">
-        <h1 class="blvck">Welcome to Flash4DAO</h1>
+        <h1>Welcome to Flash4DAO</h1>
         <button onClick={connectWithMetamask} className="btn-hero">
           Connect your wallet
         </button>
@@ -69,8 +135,31 @@ const App = () => {
 if (hasClaimedNFT) {
   return (
     <div className="member-page">
-      <h1 class="blvck">Flash4DAO Member Page</h1>
+      <h1>Flash4DAO Member Page</h1>
       <p>Congratulations on being a member</p>
+      <div>
+        <div>
+          <h2>Member List</h2>
+          <table className="card">
+            <thead>
+              <tr>
+                <th>Address</th>
+                <th>Token Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {memberList.map((member) => {
+                return (
+                  <tr key={member.address}>
+                    <td>{shortenAddress(member.address)}</td>
+                    <td>{member.tokenAmount}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
@@ -78,7 +167,7 @@ if (hasClaimedNFT) {
   // Render mint nft screen.
   return (
     <div className="mint-nft">
-      <h1>Mint your free 🍪DAO Membership NFT</h1>
+      <h1>Mint your free Flash4DAO Membership NFT</h1>
       <button
         disabled={isClaiming}
         onClick={mintNft}
